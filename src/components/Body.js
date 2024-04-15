c009533c839eadacae04837aa7354551a2104ab7
unknown <edavid.rd@gmail.com>
2024-04-15 14:05:38 -0400

    button logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,16 +2,26 @@
 import '../styles/body.css'
 import image1 from '../assets/images/image 1.png'
 import pause from '../assets/icons/Pause Button.svg'
+import play from '../assets/icons/Circled Play.svg'
 import next from '../assets/icons/Next page.svg'
 import back from '../assets/icons/Back To.svg'
+// hooks
+import { useState } from 'react'
 
 const Body = () =>
 {
+    const [pausePlay, setPausePlay] = useState(play)
     const currentExercise = "Jumping Jacks"
     const currentGoal = 50
     const previousRecord = 40
     const nextExercise = "Burpees"
     const nextGoal = 10
+    var time = pausePlay === pause ? "00:01" : ""
+    var repetitions = pausePlay === pause ? "1" : ""
+    const handleClick = () =>
+    {
+        pausePlay === pause ? setPausePlay(play) : setPausePlay(pause)
+    }
     return (
         <body className='body'>
             <div className='top-titles'>
@@ -21,7 +31,9 @@ const Body = () =>
                 <h3 id='record-variable'>{previousRecord + " " + currentExercise}</h3>
             </div>
             <div className='animations'>
+                <h1 id='timer'>{time}</h1>
                 <img className='animation' src={image1} alt='animation' />
+                <h1 id='counter'>{repetitions}</h1>
             </div>
             <div className='bottom-titles'>
                 <h3 id='next'>Next exercise: </h3>
@@ -29,7 +41,7 @@ const Body = () =>
             </div>
             <div className='controls'>
                 <img src={back} alt='back button' />
-                <img src={pause} alt='pause button' />
+                <img onClick={handleClick} src={pausePlay} alt='pause button' />
                 <img src={next} alt='next button' />
             </div>
         </body>
